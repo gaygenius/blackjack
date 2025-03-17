bd00f5a18b56b9987cc38f71a63106c056a26eb5
Jose Capo <jose@countable.com>
2025-03-17 15:42:48 -0400

    Add start action to GamesController and update play view with form for ante

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,8 @@
 class GamesController < ApplicationController
   def play
   end
+
+  def start
+    Rails.logger.info "Start action params: #{params.inspect}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: redirect("/games/play", status: 302)
 
   get "games/play"
+  post 'games/start', to: 'games#start'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
