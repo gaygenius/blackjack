2979d93269a3c6072676f1dc34657e417d04c12a
Jose Capo <jose@countable.com>
2025-03-20 12:22:49 -0400

    Add hit functionality for dealer and implement points calculation; update dealer display in game view

--- app/models/dealer.rb
+++ app/models/dealer.rb
@@ -7,4 +7,22 @@ class Dealer < ApplicationRecord
   def initialize_dealer
     self.hand = [ self.game.deck.deal, self.game.deck.deal ]
   end
+
+  def hit!
+    self.hand << self.game.deck.deal
+    self.save
+  end
+
+  def points
+    max_points = hand.map(&:points).sum
+    points = [ max_points ]
+    hand_aces.each_with_index do |_, index|
+      points << max_points - 10 * (index + 1)
+    end
+    points
+  end
+
+  def hand_aces
+    hand.select { |card| card.rank == "A" }
+  end
 end
